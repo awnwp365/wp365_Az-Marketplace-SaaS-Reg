3b47f334f0cbc2b456fbcdbca85bf760e32cc138
Andrew Stien <awn@workpoint.dk>
2024-01-12 09:42:19 +0100

    keyvault param

--- deployment/Upgrade.ps1
+++ deployment/Upgrade.ps1
@@ -7,8 +7,8 @@
 
 Param(  
    [string][Parameter(Mandatory)]$WebAppNamePrefix, # Prefix used for creating web applications
-   [string][Parameter(Mandatory)]$ResourceGroupForDeployment # Name of the resource group to deploy the resources
-   [string][Parameter()]$KeyVault, # Name of KeyVault
+   [string][Parameter(Mandatory)]$ResourceGroupForDeployment, # Name of the resource group to deploy the resources
+   [string][Parameter()]$KeyVault # Name of KeyVault
 )
 
 Function String-Between
